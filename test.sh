a1fcb51fe3a416336bfac107e88e63c907778d7e
Stephen Oliver <steve@infincia.com>
2016-12-09 02:27:38 -0500

    [test] link test against Security and Foundation frameworks

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+rm test -f &> /dev/null
 
-clang -o test src/test.c -Ldist-osx/lib -Idist-osx/include -lsqlite3 -lz -lsodium  -lsdsync -O2
+clang -o test src/test.c -Ldist-osx/lib -Idist-osx/include -lsqlite3 -lz -lsodium  -lsdsync -framework Foundation -framework Security -O2
 
 time RUST_BACKTRACE=1 ./test
 
