5ea7780e35510b52f1b307859a4e8f91bed5b0dc
Stephen Oliver <steve@infincia.com>
2016-12-09 00:42:53 -0500

    [Library] shard directories need to be lowercase

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
-pub static BLOCK_DIRECTORIES: &'static[&'static str] = &["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+pub static BLOCK_DIRECTORIES: &'static[&'static str] = &["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
 
 pub static HMAC_SIZE: i32  = 32; // 32 bytes
 pub static NONCE_SIZE: i32 = 24; // 24 bytes
