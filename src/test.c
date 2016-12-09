9334342e0de53b93970c897e830a95cac6753914
Stephen Oliver <steve@infincia.com>
2016-12-09 01:34:05 -0500

    [test] use the unique client ID generator function in C interface tests

--- src/test.c
+++ src/test.c
@@ -1,5 +1,6 @@
 #import "stdio.h"
 #import <stdlib.h>
+#import <string.h>
 #import "sdsync.h"
 
 static CContext *context = NULL;
@@ -8,8 +9,21 @@ int add(const char * name, const char * path) {
 	return sdsync_add_sync_folder(context, name, path);
 }
 
-int main ( int argc, char **argv ) {     
-    context = sdsync_initialize("/Users/steve/Library/Application Support/SafeDrive", "12345");
+int main ( int argc, char **argv ) {
+    char * unique_client_id = malloc((64 * sizeof(char)) + 1);
+
+    int ret = sdsync_get_unique_client_id("stephen@safedrive.io", &unique_client_id);
+    int l = strlen(unique_client_id);
+    printf("C<test/main>: got unique id: ");
+
+    for (int i = 0; i < l; ++i) {
+        printf("%c", unique_client_id[i]);
+    }
+    printf("\n");
+
+    context = sdsync_initialize("/Users/steve/Library/Application Support/SafeDrive", unique_client_id);
+
+    free(unique_client_id);
 
     uint8_t * main_key = "ab03c34f1ece08211fe2a8039fd6424199b3f5d7b55ff13b1134b364776c45c5";
     uint8_t * hmac_key = "63d6ff853569a0aadec5f247bba51786bb73494d1a06bdc036ebac5034a2920b";
