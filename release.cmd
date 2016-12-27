590bf62b0881434700423a2cd340ef73bc4da7e8
Stephen Oliver <steve@infincia.com>
2016-12-26 23:43:34 -0500

    [Build] initial windows build on appveyor

---
+++ release.cmd
@@ -0,0 +1,20 @@
+rmdir /s dist-win-vs2015
+
+set SODIUM_LIB_DIR=dep-win-vs2015\lib
+set SODIUM_STATIC=""
+
+set OPENSSL_DIR=%CD%\dep-win-vs2015
+set OPENSSL_STATIC=""
+
+set RUSTFLAGS=""
+
+cargo.exe build --release --verbose
+
+mkdir dist-win-vs2015
+mkdir dist-win-vs2015\lib
+mkdir dist-win-vs2015\include
+
+copy target\release\libsdsync.lib dist-win-vs2015\lib\
+
+copy dep-win-vs2015\lib\* dist-win-vs2015\lib\
+copy dep-win-vs2015\include\* dist-win-vs2015\include\
