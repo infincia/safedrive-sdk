60af48f24b1d5e126166cd3db6eb17cc996dea6d
Stephen Oliver <steve@infincia.com>
2016-12-27 00:11:47 -0500

    [Build] appveyor updated

--- release.cmd
+++ release.cmd
@@ -1,20 +1,20 @@
-rmdir /s /q dist-win-vs2015
+rmdir /s /q dist-win-%Platform%-vs2015
 
-set SODIUM_LIB_DIR=dep-win-vs2015\lib
+set SODIUM_LIB_DIR=dep-win-%Platform%-vs2015\lib
 set SODIUM_STATIC=""
 
-set OPENSSL_DIR=%CD%\dep-win-vs2015
+set OPENSSL_DIR=%CD%\dep-win-%Platform%-vs2015
 set OPENSSL_STATIC=""
 
 set RUSTFLAGS=""
 
 cargo.exe build --release --verbose
 
-mkdir dist-win-vs2015
-mkdir dist-win-vs2015\lib
-mkdir dist-win-vs2015\include
+mkdir dist-win-%Platform%-vs2015
+mkdir dist-win-%Platform%-vs2015\lib
+mkdir dist-win-%Platform%-vs2015\include
 
-copy target\release\libsdsync.lib dist-win-vs2015\lib\
+copy target\release\libsdsync.lib dist-win-%Platform%-vs2015\lib\
 
-copy dep-win-vs2015\lib\* dist-win-vs2015\lib\
-copy dep-win-vs2015\include\* dist-win-vs2015\include\
+copy dep-win-%Platform%-vs2015\lib\* dist-win-%Platform%-vs2015\lib\
+copy dep-win-%Platform%-vs2015\include\* dist-win-%Platform%-vs2015\include\
