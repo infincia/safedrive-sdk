2f7a95e0a6db80cb91c4de51d79f2be257395793
Stephen Oliver <steve@infincia.com>
2016-12-28 16:09:10 -0500

    [Build] link sqlite3 statically on windows

--- unit.cmd
+++ unit.cmd
@@ -6,15 +6,8 @@ set SODIUM_STATIC=""
 set OPENSSL_DIR=%CD%\dep-win-%BIT%-vs2015
 set OPENSSL_STATIC=""
 
-if [%BIT%] EQU [x64] (
-    echo linking 64bit sqlite
-    set SQLITE3_LIB_DIR=C:\Users\appveyor\libx64
-)
+set SQLITE3_LIB_DIR=%CD%\dep-win-%BIT%-vs2015\lib
 
-if [%BIT%] EQU [x86] (
-    echo linking 32bit sqlite
-    set SQLITE3_LIB_DIR=C:\Users\appveyor\libx86
-)
 
 
 set
