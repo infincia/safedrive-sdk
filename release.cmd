802524419f19f9d1e15df44e9292dc13e9d815b9
Stephen Oliver <steve@infincia.com>
2016-12-30 18:19:54 -0500

    [Build] use /MT for libsafedrive & native code being built with gcc-rs

--- release.cmd
+++ release.cmd
@@ -11,6 +11,8 @@ set SODIUM_STATIC=""
 
 set SQLITE3_LIB_DIR=%CD%\dep-%TARGET%-%TOOLSET%\lib
 
+set RUSTFLAGS=-Z unstable-options -C target-feature=+crt-static
+
 pushd libsafedrive
 cargo.exe build --release --verbose
 popd
