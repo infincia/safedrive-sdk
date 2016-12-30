663bc01f18a272b715c853a87ad48260f5b056d4
Stephen Oliver <steve@infincia.com>
2016-12-29 20:54:49 -0500

    [Library] the account/status field “status” isn’t always present

--- libsafedrive/src/sdapi.rs
+++ libsafedrive/src/sdapi.rs
@@ -31,7 +31,7 @@ pub struct UniqueClientID {
 #[allow(non_snake_case)]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AccountStatus {
-    pub status: String,
+    pub status: Option<String>,
     pub host: String,
     pub port: u16,
     pub userName: String,
