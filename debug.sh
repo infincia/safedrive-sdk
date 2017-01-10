7cb79f93366ffc9317e3eedfaae6e7442d49e363
Stephen Oliver <steve@infincia.com>
2017-01-10 00:39:18 -0500

    [Build] add QEMU handling to build scripts

--- debug.sh
+++ debug.sh
@@ -4,6 +4,22 @@ set -ex
 
 TARGET=`rustup show | awk 'match($0, /Default host: ([0-9a-zA-Z\_]).+/) { ver = substr($3, RSTART, RLENGTH); print ver;}'`
 
+case $TARGET in
+    # configure emulation for transparent execution of foreign binaries
+    aarch64-unknown-linux-gnu)
+        export QEMU_LD_PREFIX=/usr/aarch64-linux-gnu
+        ;;
+    arm*-unknown-linux-gnueabihf)
+        export QEMU_LD_PREFIX=/usr/arm-linux-gnueabihf
+        ;;
+    *)
+        ;;
+esac
+
+if [ ! -z "$QEMU_LD_PREFIX" ]; then
+    # Run tests on a single thread when using QEMU user emulation
+    export RUST_TEST_THREADS=1
+fi
 
 rm -rf dist-$TARGET
 mkdir -p dist-$TARGET/lib
--- unit.sh
+++ unit.sh
@@ -4,6 +4,23 @@ set -ex
 
 TARGET=`rustup show | awk 'match($0, /Default host: ([0-9a-zA-Z\_]).+/) { ver = substr($3, RSTART, RLENGTH); print ver;}'`
 
+case $TARGET in
+    # configure emulation for transparent execution of foreign binaries
+    aarch64-unknown-linux-gnu)
+        export QEMU_LD_PREFIX=/usr/aarch64-linux-gnu
+        ;;
+    arm*-unknown-linux-gnueabihf)
+        export QEMU_LD_PREFIX=/usr/arm-linux-gnueabihf
+        ;;
+    *)
+        ;;
+esac
+
+if [ ! -z "$QEMU_LD_PREFIX" ]; then
+    # Run tests on a single thread when using QEMU user emulation
+    export RUST_TEST_THREADS=1
+fi
+
 export SODIUM_LIB_DIR=$PWD/dep-$TARGET/lib
 export SODIUM_STATIC
 
