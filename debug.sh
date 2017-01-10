49723c72d61d7711a1bf83eef0b700905defbb4d
Stephen Oliver <steve@infincia.com>
2017-01-10 13:13:46 -0500

    [Build] travis update

--- debug.sh
+++ debug.sh
@@ -2,9 +2,11 @@
 
 set -ex
 
-rustup default nightly
+if [ -z "${TARGET}" ]; then
+    export TARGET=`rustup show | awk 'match($0, /Default host: ([0-9a-zA-Z\_]).+/) { ver = substr($3, RSTART, RLENGTH); print ver;}'`
+fi
 
-TARGET=`rustup show | awk 'match($0, /Default host: ([0-9a-zA-Z\_]).+/) { ver = substr($3, RSTART, RLENGTH); print ver;}'`
+echo "Building for $TARGET"
 
 case $TARGET in
     # configure emulation for transparent execution of foreign binaries
