4ac24c38e2ccb187c811be7cdfdc81b650c4ba50
Stephen Oliver <steve@infincia.com>
2017-01-10 00:38:38 -0500

    [Build] use the same script settings on release/debug/unit scripts

--- debug.sh
+++ debug.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 TARGET=`rustup show | awk 'match($0, /Default host: ([0-9a-zA-Z\_]).+/) { ver = substr($3, RSTART, RLENGTH); print ver;}'`
 
--- unit.sh
+++ unit.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -ex
+
 TARGET=`rustup show | awk 'match($0, /Default host: ([0-9a-zA-Z\_]).+/) { ver = substr($3, RSTART, RLENGTH); print ver;}'`
 
 export SODIUM_LIB_DIR=$PWD/dep-$TARGET/lib
