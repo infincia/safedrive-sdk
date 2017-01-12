d5ad7134dcfa58f720b9a3f03b6b2ec2a02e3061
Stephen Oliver <steve@infincia.com>
2017-01-12 17:37:16 -0500

    [Build] always try to update rustc/cargo/libstd before a build

--- ci/install.sh
+++ ci/install.sh
@@ -21,7 +21,7 @@ install_rustup() {
     sh ~/rust/lib/rustlib/uninstall.sh
     echo "Using Rust $TRAVIS_RUST_VERSION"
     curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=$TRAVIS_RUST_VERSION
-
+    rustup update
     rustc -V
     cargo -V
 }
