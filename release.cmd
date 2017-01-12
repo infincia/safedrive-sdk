0b81f693f4115e89eaf9dc3ea68e29d0ee88369e
Stephen Oliver <steve@infincia.com>
2017-01-12 01:14:55 -0500

    [Build] windows fix

--- release.cmd
+++ release.cmd
@@ -5,7 +5,7 @@ mkdir dist-%TARGET%-%TOOLSET%-%LINKTYPE%\lib
 mkdir dist-%TARGET%-%TOOLSET%-%LINKTYPE%\include
 mkdir dist-%TARGET%-%TOOLSET%-%LINKTYPE%\bin
 
-set SODIUM_LIB_DIR=%CD%\dep-%TARGET%-%TOOLSET%-%LINKTYPE%\lib
+set SODIUM_LIB_DIR=%CD%\dep\%TARGET%\%TOOLSET%\%LINKTYPE%\lib
 
 IF "%LINKTYPE%"=="mt" (
     set SODIUM_STATIC=""
