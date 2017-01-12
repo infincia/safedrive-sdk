7508a1c7ba5dda729a797d9027937f93eda94aae
Stephen Oliver <steve@infincia.com>
2017-01-12 18:43:44 -0500

    [Test] update C binary build flags

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 rm test -f &> /dev/null
 
-clang -o test libsafedrive/src/test.c -Ldist-x86_64-apple-darwin/dep/lib -Ldist-x86_64-apple-darwin/lib -Idist-x86_64-apple-darwin/include -lz -lsodium -lsafedrive -framework Foundation -framework Security -O2
+clang -o test libsafedrive/src/test.c -rpath dist-x86_64-apple-darwin/lib -Ldist-x86_64-apple-darwin/lib -Idist-x86_64-apple-darwin/include -lz -lsodium -lsafedrive -framework Foundation -framework Security -O2
 
 time RUST_BACKTRACE=1 ./test
 
