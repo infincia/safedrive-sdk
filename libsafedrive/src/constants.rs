00567d66b6d8dde9482ff085607a9fc8f132b72c
Stephen Oliver <steve@infincia.com>
2017-01-17 15:21:44 -0500

    [Lint] removed unused constant

--- libsafedrive/src/constants.rs
+++ libsafedrive/src/constants.rs
@@ -1,5 +1,3 @@
-pub static BLOCK_DIRECTORIES: &'static[&'static str] = &["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
-
 pub static DEFAULT_PERMISSIONS: i32 = 493;
 
 pub static DEBUG_STATISTICS: bool = false;
