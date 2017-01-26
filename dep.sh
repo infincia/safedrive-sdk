c2cb1d4eefadcc49d1fb4c5ba3e0db867be524bc
Stephen Oliver <steve@infincia.com>
2017-01-26 17:50:10 -0500

    [Build] add build support for openssl during build time
    Allows for static linking with musl

--- dep.sh
+++ dep.sh
@@ -7,12 +7,14 @@ case $TARGET in
         export CFLAGS="-O2 -g -flto"
         export LDFLAGS="-flto"
         export CONFIGURE_ARGS="--enable-shared=yes"
+        export OPENSSL_ARGS="shared"
         ;;
     arm*-unknown-linux-gnueabihf)
         export QEMU_LD_PREFIX=/usr/arm-linux-gnueabihf
         export CFLAGS="-O2 -g -flto"
         export LDFLAGS="-flto"
         export CONFIGURE_ARGS="--enable-shared=yes"
+        export OPENSSL_ARGS="shared"
         ;;
     x86_64-apple-darwin)
         export OSX_VERSION_MIN=${OSX_VERSION_MIN-"10.9"}
@@ -21,17 +23,20 @@ case $TARGET in
         export LDFLAGS="-arch x86_64 -mmacosx-version-min=${OSX_VERSION_MIN} -march=${OSX_CPU_ARCH} -flto"
         export RUSTFLAGS="-C link-args=-mmacosx-version-min=10.9"
         export CONFIGURE_ARGS="--enable-shared=yes"
+        export OPENSSL_ARGS="shared"
         ;;
     x86_64-unknown-linux-gnu|i686-unknown-linux-gnu)
         export CFLAGS="-O2 -g -flto"
         export LDFLAGS="-flto"
         export CONFIGURE_ARGS="--enable-shared=yes"
+        export OPENSSL_ARGS="shared"
         ;;
     x86_64-unknown-linux-musl|i686-unknown-linux-musl)
         export CFLAGS="-O2 -g -flto"
         export LDFLAGS="-flto"
         export CC=musl-gcc
         export CONFIGURE_ARGS="--enable-shared=no"
+        export OPENSSL_ARGS="no-shared"
         ;;
     *)
         ;;
@@ -40,6 +45,21 @@ esac
 if [ ! -z "$QEMU_LD_PREFIX" ]; then
     # Run tests on a single thread when using QEMU user emulation
     export RUST_TEST_THREADS=1
+fi
+
+if [ ! -f dep/$TARGET/lib/libssl.a ]; then
+    export OPENSSL_VER=1.1.0d
+
+    wget https://www.openssl.org/source/openssl-$OPENSSL_VER.tar.gz
+    tar xvzf openssl-$OPENSSL_VER.tar.gz
+    OPENSSL_PREFIX=$PWD/dep/$TARGET
+    pushd openssl-$OPENSSL_VER
+    ./config --prefix=$OPENSSL_PREFIX $OPENSSL_ARGS
+    env C_INCLUDE_PATH=$OPENSSL_PREFIX/include/ make depend
+    make
+    make install
+    popd
+    rm -rf openssl*
 fi
 
 if [ ! -f dep/$TARGET/lib/libsodium.a ]; then
