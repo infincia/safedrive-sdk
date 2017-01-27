620ca864db5bd89d7feb0c8664b55c14c4471e99
Stephen Oliver <steve@infincia.com>
2017-01-26 19:00:15 -0500

    [Build] ensure travis installs the correct rust toolchain

--- ci/install.sh
+++ ci/install.sh
@@ -20,7 +20,7 @@ install_rustup() {
     # uninstall the rust toolchain installed by travis, we are going to use rustup
     sh ~/rust/lib/rustlib/uninstall.sh
     echo "Using Rust $TRAVIS_RUST_VERSION"
-    curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=$TRAVIS_RUST_VERSION
+    curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=$TRAVIS_RUST_VERSION-$TARGET
     rustc -V
     cargo -V
     cargo install rusty-cheddar
