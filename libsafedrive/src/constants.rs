0d34e2f9d35dae5140d88a75db3b3240a57f89ba
Stephen Oliver <steve@infincia.com>
2017-02-02 18:30:54 -0500

    [Library] add central accessor for api domain

--- libsafedrive/src/constants.rs
+++ libsafedrive/src/constants.rs
@@ -12,8 +12,8 @@ pub static DEBUG_STATISTICS: bool = true;
 static SD_WEB_DOMAIN_PRODUCTION: &'static str = "safedrive.io";
 static SD_WEB_DOMAIN_STAGING: &'static str = "staging.safedrive.io";
 
-pub static SDAPIDOMAIN_STAGING: &'static str = "staging.safedrive.io";
-pub static SDAPIDOMAIN_PRODUCTION: &'static str = "safedrive.io";
+static SD_API_DOMAIN_PRODUCTION: &'static str = "safedrive.io";
+static SD_API_DOMAIN_STAGING: &'static str = "staging.safedrive.io";
 
 pub static SDGROUP_NAME: &'static str = "group.io.safedrive.db";
 
@@ -38,5 +38,13 @@ pub fn web_domain() -> &'static str {
         SD_WEB_DOMAIN_PRODUCTION
     } else {
         SD_WEB_DOMAIN_STAGING
+    }
+}
+
+pub fn api_domain() -> &'static str {
+    if is_production() {
+        SD_API_DOMAIN_PRODUCTION
+    } else {
+        SD_API_DOMAIN_STAGING
     }
 }
