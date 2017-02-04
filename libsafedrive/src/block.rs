d1fbc6c41d7b9453a395fc6ac7557d6e184b7083
Stephen Oliver <steve@infincia.com>
2017-02-04 17:15:50 -0500

    [Library] refactor block/session key wrapping and nonce handling
    Use Key/WrappedKey when handling them inside a Block or Session, and let
    them handle wrapping/unwrapping themselves
    
    Use Nonce type inside Block and Session

--- libsafedrive/src/block.rs
+++ libsafedrive/src/block.rs
@@ -40,8 +40,8 @@ impl Block {
             _ => {
                 panic!("Attempted to create invalid block version");
             },
-
         };
+
         Block { version: version, data: data, hmac: block_hmac }
     }
 
@@ -56,33 +56,42 @@ impl Block {
     pub fn to_wrapped(self, main: &Key) -> Result<WrappedBlock, CryptoError> {
 
         // generate a new block key
-        let block_key_raw = ::sodiumoxide::randombytes::randombytes(SECRETBOX_KEY_SIZE);
-        let block_key_struct = ::sodiumoxide::crypto::secretbox::Key::from_slice(&block_key_raw)
-            .expect("failed to get block key struct");
-
-        // We use the first 24 bytes of the block hmac value as nonce for wrapping
-        // the block key and encrypting the block itself.
-        //
-        // This is cryptographically safe but still deterministic: encrypting
-        // the same block twice with a specific key will always produce the same
-        // output block, which is critical for versioning and deduplication
-        // across all backups of all sync folders
-        let nonce = ::sodiumoxide::crypto::secretbox::Nonce::from_slice(&self.hmac.as_slice()[0..SECRETBOX_NONCE_SIZE as usize])
-            .expect("failed to get nonce");
+        let block_key = Key::new(KeyType::Block);
 
         // we use the same nonce both while wrapping the block key, and the block data itself
         // this is safe because using the same nonce with 2 different keys is not nonce reuse
+        let block_nonce = match self.version {
+            SyncVersion::Version1 => {
+                // We use the first 24 bytes of the block hmac value as nonce for wrapping
+                // the block key and encrypting the block itself.
+                //
+                // This is cryptographically safe but still deterministic: encrypting
+                // the same block twice with a specific key will always produce the same
+                // output block, which is critical for versioning and deduplication
+                // across all backups of all sync folders
+                ::sodiumoxide::crypto::secretbox::Nonce::from_slice(&self.hmac.as_slice()[0..SECRETBOX_NONCE_SIZE as usize])
+                    .expect("failed to get nonce")
+            },
+            SyncVersion::Version2 => {
+                // We use the blake2 hash function to generate exactly 192-bits/24 bytes
+                panic!("Version 2 unimplemented");
+            },
+            _ => {
+                panic!("Attempted to use secretbox nonce with invalid block version");
+            },
+        };
 
         // encrypt the block data using the block key
-        let wrapped_data = ::sodiumoxide::crypto::secretbox::seal(self.data.as_slice(), &nonce, &block_key_struct);
+        let wrapped_data = ::sodiumoxide::crypto::secretbox::seal(self.data.as_slice(), &block_nonce, &block_key.as_sodium_secretbox_key());
 
         // wrap the block key with the main encryption key
-        let wrapped_block_key_raw = ::sodiumoxide::crypto::secretbox::seal(&block_key_raw, &nonce, &(main.as_sodium_secretbox_key()));
-        assert!(wrapped_block_key_raw.len() == SECRETBOX_KEY_SIZE + SECRETBOX_MAC_SIZE);
-        let wrapped_block_key = WrappedKey::new(wrapped_block_key_raw, KeyType::Session);
+        let wrapped_block_key = match block_key.to_wrapped(main, Some(&block_nonce)) {
+            Ok(wk) => wk,
+            Err(e) => return Err(e),
+        };
 
 
-        Ok(WrappedBlock { version: self.version, hmac: self.hmac, wrapped_data: wrapped_data, wrapped_key: wrapped_block_key })
+        Ok(WrappedBlock { version: self.version, hmac: self.hmac, wrapped_data: wrapped_data, wrapped_key: wrapped_block_key, nonce: block_nonce })
     }
 }
 
@@ -93,26 +102,20 @@ pub struct WrappedBlock {
     pub version: SyncVersion,
     pub hmac: Vec<u8>,
     pub wrapped_data: Vec<u8>,
+    nonce: ::sodiumoxide::crypto::secretbox::Nonce,
     wrapped_key: WrappedKey,
 }
 
 
 impl WrappedBlock {
     pub fn to_block(self, main: &Key) -> Result<Block, SDError> {
-        let nonce = ::sodiumoxide::crypto::secretbox::Nonce::from_slice(&self.hmac)
-            .expect("failed to get nonce");
 
-        let main_key_s = ::sodiumoxide::crypto::secretbox::Key::from_slice(main.as_ref())
-            .expect("failed to get main key struct");
-
-        let block_key_raw = match ::sodiumoxide::crypto::secretbox::open(self.wrapped_key.as_ref(), &nonce, &main_key_s) {
+        let block_key = match self.wrapped_key.to_key(main, Some(&self.nonce)) {
             Ok(k) => k,
             Err(_) => return Err(SDError::CryptoError(CryptoError::BlockDecryptFailed))
         };
 
-        let block_key_s = ::sodiumoxide::crypto::secretbox::Key::from_slice(&block_key_raw).expect("failed to get unwrapped block key struct");
-
-        let block_raw = match ::sodiumoxide::crypto::secretbox::open(&self.wrapped_data, &nonce, &block_key_s) {
+        let block_raw = match ::sodiumoxide::crypto::secretbox::open(&self.wrapped_data, &self.nonce, &block_key.as_sodium_secretbox_key()) {
             Ok(s) => s,
             Err(_) => return Err(SDError::CryptoError(CryptoError::BlockDecryptFailed))
         };
@@ -138,8 +141,9 @@ impl WrappedBlock {
         // next 48 bytes will be the wrapped block key
         binary_data.extend(self.wrapped_key.as_ref());
 
-        // next 24 bytes will be the nonce/hmac
-        binary_data.extend(self.hmac.as_slice());
+        // next 24 bytes will be the nonce
+        let n: &[u8] = self.nonce.as_ref();
+        binary_data.extend(n);
         assert!(binary_data.len() == magic.len() + file_type.len() + version.len() + reserved.len() + (SECRETBOX_KEY_SIZE + SECRETBOX_MAC_SIZE) + SECRETBOX_NONCE_SIZE);
 
         // remainder will be the encrypted block data
@@ -164,12 +168,16 @@ impl WrappedBlock {
             _ => panic!("unknown binary version")
         };
         let wrapped_block_key_raw = raw_block.wrapped_key.to_vec();
-        // let nonce_raw = raw_block.nonce.to_vec();
+        let nonce_raw = raw_block.nonce;
+
+        let block_nonce = ::sodiumoxide::crypto::secretbox::Nonce::from_slice(&nonce_raw)
+            .expect("failed to get nonce");
+
         let wrapped_block_raw = raw_block.wrapped_data.to_vec();
 
         let wrapped_block_key = WrappedKey::from(wrapped_block_key_raw, KeyType::Block);
 
-        Ok(WrappedBlock { version: block_ver, hmac: hmac, wrapped_key: wrapped_block_key, wrapped_data: wrapped_block_raw })
+        Ok(WrappedBlock { version: block_ver, hmac: hmac, wrapped_key: wrapped_block_key, wrapped_data: wrapped_block_raw, nonce: block_nonce })
     }
 
 }
--- libsafedrive/src/session.rs
+++ libsafedrive/src/session.rs
@@ -37,29 +37,24 @@ impl SyncSession {
     pub fn to_wrapped(self, main: &Key) -> Result<WrappedSyncSession, CryptoError> {
 
         // generate a new session key
-        let session_key_raw = ::sodiumoxide::randombytes::randombytes(SECRETBOX_KEY_SIZE);
-        let session_key_struct = ::sodiumoxide::crypto::secretbox::Key::from_slice(&session_key_raw)
-            .expect("failed to get session key struct");
+        let session_key = Key::new(KeyType::Session);
 
         // We use a random nonce here because we don't need to know what it is in advance, unlike blocks
-        let nonce_raw = ::sodiumoxide::randombytes::randombytes(SECRETBOX_NONCE_SIZE);
-
-        let nonce = ::sodiumoxide::crypto::secretbox::Nonce::from_slice(&nonce_raw)
-            .expect("failed to get nonce");
+        let session_nonce = ::sodiumoxide::crypto::secretbox::gen_nonce();
 
         // we use the same nonce both while wrapping the session key, and the session data itself
         // this is safe because using the same nonce with 2 different keys is not nonce reuse
 
         // encrypt the session data using the session key
-        let wrapped_data = ::sodiumoxide::crypto::secretbox::seal(&self.data, &nonce, &session_key_struct);
+        let wrapped_data = ::sodiumoxide::crypto::secretbox::seal(&self.data, &session_nonce, &session_key.as_sodium_secretbox_key());
 
         // wrap the session key with the main encryption key
-        let wrapped_session_key_raw = ::sodiumoxide::crypto::secretbox::seal(&session_key_raw, &nonce, &(main.as_sodium_secretbox_key()));
-        assert!(wrapped_session_key_raw.len() == SECRETBOX_KEY_SIZE + SECRETBOX_MAC_SIZE);
-        let wrapped_session_key = WrappedKey::new(wrapped_session_key_raw, KeyType::Session);
+        let wrapped_session_key = match session_key.to_wrapped(main, Some(&session_nonce)) {
+            Ok(wk) => wk,
+            Err(e) => return Err(e),
+        };
 
-
-        Ok(WrappedSyncSession { version: self.version, folder_id: self.folder_id, name: self.name, size: self.size, time: self.time, wrapped_data: wrapped_data, wrapped_key: wrapped_session_key, nonce: nonce_raw })
+        Ok(WrappedSyncSession { version: self.version, folder_id: self.folder_id, name: self.name, size: self.size, time: self.time, wrapped_data: wrapped_data, wrapped_key: wrapped_session_key, nonce: session_nonce })
     }
 }
 
@@ -73,27 +68,20 @@ pub struct WrappedSyncSession {
     pub size: Option<u64>,
     pub time: Option<u64>,
     pub wrapped_data: Vec<u8>,
-    nonce: Vec<u8>,
+    nonce: ::sodiumoxide::crypto::secretbox::Nonce,
     wrapped_key: WrappedKey,
 }
 
 
 impl WrappedSyncSession {
     pub fn to_session(self, main: &Key) -> Result<SyncSession, SDError> {
-        let nonce = ::sodiumoxide::crypto::secretbox::Nonce::from_slice(&self.nonce)
-            .expect("failed to get nonce");
 
-        let main_key_s = ::sodiumoxide::crypto::secretbox::Key::from_slice(main.as_ref())
-            .expect("failed to get main key struct");
-
-        let session_key_raw = match ::sodiumoxide::crypto::secretbox::open(self.wrapped_key.as_ref(), &nonce, &main_key_s) {
+        let session_key = match self.wrapped_key.to_key(main, Some(&self.nonce)) {
             Ok(k) => k,
             Err(_) => return Err(SDError::CryptoError(CryptoError::SessionDecryptFailed))
         };
 
-        let session_key_s = ::sodiumoxide::crypto::secretbox::Key::from_slice(&session_key_raw).expect("failed to get unwrapped session key struct");
-
-        let session_raw = match ::sodiumoxide::crypto::secretbox::open(&self.wrapped_data, &nonce, &session_key_s) {
+        let session_raw = match ::sodiumoxide::crypto::secretbox::open(&self.wrapped_data, &self.nonce, &session_key.as_sodium_secretbox_key()) {
             Ok(s) => s,
             Err(_) => return Err(SDError::CryptoError(CryptoError::SessionDecryptFailed))
         };
@@ -121,7 +109,7 @@ impl WrappedSyncSession {
         binary_data.extend(self.wrapped_key.as_ref());
 
         // next 24 bytes will be the nonce
-        binary_data.extend(self.nonce);
+        binary_data.extend(self.nonce.as_ref());
         assert!(binary_data.len() == magic.len() + file_type.len() + version.len() + reserved.len() + (SECRETBOX_KEY_SIZE + SECRETBOX_MAC_SIZE) + SECRETBOX_NONCE_SIZE);
 
         // remainder will be the encrypted session data
@@ -146,12 +134,16 @@ impl WrappedSyncSession {
             _ => panic!("Invalid binary session version")
         };
         let wrapped_session_key_raw = raw_session.wrapped_key.to_vec();
-        let nonce_raw = raw_session.nonce.to_vec();
+        let nonce_raw = raw_session.nonce;
+
+        let session_nonce = ::sodiumoxide::crypto::secretbox::Nonce::from_slice(&nonce_raw)
+            .expect("failed to get nonce");
+
         let wrapped_session_raw = raw_session.wrapped_data.to_vec();
 
         let wrapped_session_key = WrappedKey::from(wrapped_session_key_raw, KeyType::Session);
 
-        Ok(WrappedSyncSession { version: session_ver, folder_id: Some(body.folder_id), name: body.name.to_string(), size: None, time: None, wrapped_key: wrapped_session_key, wrapped_data: wrapped_session_raw, nonce: nonce_raw })
+        Ok(WrappedSyncSession { version: session_ver, folder_id: Some(body.folder_id), name: body.name.to_string(), size: None, time: None, wrapped_key: wrapped_session_key, wrapped_data: wrapped_session_raw, nonce: session_nonce })
     }
 
 }
