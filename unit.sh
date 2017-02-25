1216e20f82f4f4eb53ad1a3efbe7497ae4d33066
Stephen Oliver <steve@infincia.com>
2017-02-24 20:40:10 -0500

    [Build] don’t print commands invocations in shell scripts

--- unit.sh
+++ unit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 if [ -z "${TARGET}" ]; then
     export TARGET=`rustup show | awk 'match($0, /Default host: ([0-9a-zA-Z\_]).+/) { ver = substr($3, RSTART, RLENGTH); print ver;}'`
