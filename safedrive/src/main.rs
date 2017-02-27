11d581f084e6f63f6fcd4d449767c358d129a530
Stephen Oliver <steve@infincia.com>
2017-02-26 23:03:52 -0500

    [Binary] don’t look for openssl roots dir at launch on linux

--- safedrive/src/main.rs
+++ safedrive/src/main.rs
@@ -243,33 +243,6 @@ fn main() {
 
     let _ = TermLogger::init(log_level, Config::default()).unwrap();
 
-    #[cfg(target_os = "linux")]
-    {
-        match get_openssl_directory() {
-            Ok(ssldir) => {
-                debug!("Using openssl dir: {:?}", &ssldir);
-
-                let mut cert_dir = PathBuf::from(&ssldir);
-                cert_dir.push("certs");
-                let cert_dir_r: &OsStr = cert_dir.as_ref();
-
-                let mut cert_file = PathBuf::from(&ssldir);
-                cert_file.push("certs");
-                cert_file.push("ca-certificates.crt");
-                let cert_file_r: &OsStr = cert_file.as_ref();
-
-                env::set_var("SSL_CERT_DIR", cert_dir_r);
-
-                env::set_var("SSL_CERT_FILE", cert_file_r);
-
-            },
-            Err(_) => {
-                error!("Could not find openssl certificate store");
-                std::process::exit(1);
-            }
-        };
-    }
-
     println!("{} {}", NAME, VERSION);
     println!("{}", COPYRIGHT);
     println!();
