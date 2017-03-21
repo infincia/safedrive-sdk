e8f6f33590489deb2b308bf6a9ea522204764375
Stephen Oliver <steve@infincia.com>
2017-03-21 16:58:16 -0400

    [Wrapper] add error domains in new constants file in Swift

---
+++ Wrappers/Swift/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SafeDriveSDK
+//
+//  Created by steve on 3/21/17.
+//  Copyright © 2017 SafeDrive. All rights reserved.
+//
+
+import Foundation
+
+public let SDErrorDomainNotReported = "io.safedrive.notreported"
+public let SDErrorDomainReported = "io.safedrive.reported"
+public let SDErrorDomainInternal = "io.safedrive.internal"
