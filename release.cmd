adae21518ec34090a0d87a21944ff33a51683f94
Stephen Oliver <steve@infincia.com>
2017-03-29 17:24:18 -0400

    [Build] set a library prefix on windows where one applies

--- release.cmd
+++ release.cmd
@@ -3,6 +3,11 @@ IF [%TARGET%]==[] set TARGET=x86_64-pc-windows-msvc
 IF [%TOOLSET%]==[] set TOOLSET=v141
 IF [%LINKTYPE%]==[] set LINKTYPE=dll
 
+set LIBPREFIX=
+IF [%TOOLSET%]==[v141] set LIBPREFIX=lib
+
+
+
 ECHO building SafeDrive for Windows-%ARCH%
 
 del /q dist-%TARGET%-%TOOLSET%-%LINKTYPE%
@@ -29,12 +34,11 @@ cheddar -f libsafedrive\src\c_api.rs dist-%TARGET%-%TOOLSET%-%LINKTYPE%\include\
 
 cargo.exe build --release -p safedrive --target %TARGET%
 
-copy /y dep\%TARGET%\%TOOLSET%\%LINKTYPE%\lib\libsodium.dll %CD%\dist-%TARGET%-%TOOLSET%-%LINKTYPE%\lib\libsodium.dll
-copy /y dep\%TARGET%\%TOOLSET%\%LINKTYPE%\lib\libsodium.lib %CD%\dist-%TARGET%-%TOOLSET%-%LINKTYPE%\lib\libsodium.lib
+copy /y dep\%TARGET%\%TOOLSET%\%LINKTYPE%\lib\%LIBPREFIX%sodium.dll dist-%TARGET%-%TOOLSET%-%LINKTYPE%\lib\%LIBPREFIX%sodium.dll
+copy /y dep\%TARGET%\%TOOLSET%\%LINKTYPE%\lib\%LIBPREFIX%sodium.lib dist-%TARGET%-%TOOLSET%-%LINKTYPE%\lib\%LIBPREFIX%sodium.lib
 
-copy /y target\%TARGET%\release\safedrive.lib dist-%TARGET%-%TOOLSET%-%LINKTYPE%\lib\safedrive.lib
-copy /y target\%TARGET%\release\safedrive.dll dist-%TARGET%-%TOOLSET%-%LINKTYPE%\lib\safedrive.dll
+copy /y target\%TARGET%\release\safedrive.dll dist-%TARGET%-%TOOLSET%-%LINKTYPE%\lib\%LIBPREFIX%safedrive.dll
 
 copy /y target\%TARGET%\release\safedrive.exe dist-%TARGET%-%TOOLSET%-%LINKTYPE%\bin\
 
-copy /y dist-%TARGET%-%TOOLSET%-%LINKTYPE%\lib\libsodium.dll dist-%TARGET%-%TOOLSET%-%LINKTYPE%\bin\libsodium.dll
+copy /y dist-%TARGET%-%TOOLSET%-%LINKTYPE%\lib\%LIBPREFIX%sodium.dll dist-%TARGET%-%TOOLSET%-%LINKTYPE%\bin\%LIBPREFIX%sodium.dll
