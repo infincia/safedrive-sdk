9861e5777d876574e232a7e58aeb31fda5f313ab
Stephen Oliver <steve@infincia.com>
2017-03-30 01:57:00 -0400

    [Build] use consistent build messages on windows

--- unit.cmd
+++ unit.cmd
@@ -3,7 +3,7 @@ IF [%TARGET%]==[] set TARGET=x86_64-pc-windows-msvc
 IF [%TOOLSET%]==[] set TOOLSET=v141_xp
 IF [%LINKTYPE%]==[] set LINKTYPE=static
 
-ECHO testing SafeDrive for Windows-%ARCH%
+ECHO testing safedrive for %TARGET% (%TOOLSET%-%LINKTYPE%)
 
 set SODIUM_LIB_DIR=%CD%\dep\%TARGET%\%TOOLSET%\%LINKTYPE%\lib
 set RUST_BACKTRACE="1"
