b7b966d9c98d514bba3a48f11b9423663a7691b6
Stephen Oliver <steve@infincia.com>
2017-04-01 20:16:13 -0400

    [Wrapper] add log level enum to Swift, matches libsafedrive levels

--- Wrappers/Swift/Constants.swift
+++ Wrappers/Swift/Constants.swift
@@ -11,3 +11,11 @@ import Foundation
 public let SDErrorDomainNotReported = "io.safedrive.notreported"
 public let SDErrorDomainReported = "io.safedrive.reported"
 public let SDErrorDomainInternal = "io.safedrive.internal"
+
+public enum SDKLogLevel: UInt8 {
+    case error = 0
+    case warn = 1
+    case info = 2
+    case debug = 3
+    case trace = 4
+}
