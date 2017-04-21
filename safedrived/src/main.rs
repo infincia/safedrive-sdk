36a46079865db1407709803c6df499f650287682
Stephen Oliver <steve@infincia.com>
2017-04-21 19:01:35 -0400

    [Daemon] replace tokio inotify with the notify crate

--- safedrived/src/main.rs
+++ safedrived/src/main.rs
@@ -5,37 +5,26 @@ extern crate simplelog;
 use simplelog::{Config, TermLogger};
 
 #[macro_use]
-extern crate prettytable;
-use prettytable::Table;
-use prettytable::row::Row;
-use prettytable::cell::Cell;
-
-#[macro_use]
 extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
-use std::str;
-use std::fs::File;
-
 use std::path::{Path, PathBuf};
-use std::io::Read;
 
 extern crate clap;
 use clap::{Arg, App, SubCommand};
 
-extern crate futures;
-extern crate tokio_inotify;
-extern crate tokio_core;
-use futures::stream::Stream;
-use tokio_inotify::AsyncINotify;
-use tokio_core::reactor::Core;
+extern crate notify;
 
+use notify::{RecommendedWatcher, Watcher, RecursiveMode, DebouncedEvent};
+use std::sync::mpsc::channel;
+use std::time::Duration;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 const NAME: &'static str = env!("CARGO_PKG_NAME");
 const COPYRIGHT: &'static str = "(C) 2013-2017 SafeData Net S.R.L.";
 
+const DEBOUNCE_DURATION: u64 = 2;
 
 #[derive(Debug)]
 pub enum Configuration {
@@ -110,29 +99,104 @@ fn main() {
         let pa = PathBuf::from(&p);
 
         println!("Registering for filesystem events: {:?}",  &pa.file_name().unwrap().to_str().unwrap());
-        let mut evloop = match Core::new() {
-            Ok(evloop) => evloop,
-            Err(e) => panic!("Failed to create event loop: {}", e),
-        };
+        match watch(&pa) {
+            Ok(()) => {},
+            Err(notify::Error::Generic(ref string)) => panic!("Failed to watch: {}", string),
+            Err(notify::Error::Io(ioerror)) => panic!("Failed to watch: {}", ioerror),
+            Err(notify::Error::PathNotFound) => panic!("Failed to watch: path not found: {}", &pa.display()),
+            Err(notify::Error::WatchNotFound) => panic!("Failed to unwatch, not currently watching"),
+        }
+    }
+}
 
-        let inot = match AsyncINotify::init(&evloop.handle()) {
-            Ok(inot) => inot,
-            Err(e) => panic!("Failed to get inotify handle: {}", e),
-        };
 
+fn watch(path: &Path) -> notify::Result<()> {
+    let (tx, rx) = channel();
+    let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(DEBOUNCE_DURATION))?;
+    watcher.watch(path, RecursiveMode::Recursive)?;
+    loop {
+        match rx.recv() {
+            Ok(event) => {
+                match event {
+                    /// `NoticeWrite` is emitted immediately after the first write event for the path.
+                    ///
+                    /// If you are reading from that file, you should probably close it immediately and discard all
+                    /// data you read from it.
+                    DebouncedEvent::NoticeWrite(path) => {
+                        println!("notice write: {}", path.display());
+                    },
 
-        match inot.add_watch(&pa, tokio_inotify::IN_DELETE) {
-            Ok(r) => {},
-            Err(e) => panic!("Failed to register for folder events: {}", e),
-        };
+                    /// `NoticeRemove` is emitted immediately after a remove or rename event for the path.
+                    ///
+                    /// The file will continue to exist until its last file handle is closed.
+                    DebouncedEvent::NoticeRemove(path) => {
+                        println!("notice remove: {}", path.display());
+                    },
 
-        let events = inot.for_each(|ev| {
-            if ev.is_delete() {
-                println!("EV: item deleted {:?}", ev.name);
-            }
-            Ok(())
-        });
+                    /// `Create` is emitted when a file or directory has been created and no events were detected
+                    /// for the path within the specified time frame.
+                    ///
+                    /// `Create` events have a higher priority than `Write` and `Chmod`. These events will not be
+                    /// emitted if they are detected before the `Create` event has been emitted.
+                    DebouncedEvent::Create(path) => {
+                        println!("create: {}", path.display());
+                    },
 
-        evloop.run(events).unwrap();
+                    /// `Write` is emitted when a file has been written to and no events were detected for the path
+                    /// within the specified time frame.
+                    ///
+                    /// `Write` events have a higher priority than `Chmod`. `Chmod` will not be emitted if it's
+                    /// detected before the `Write` event has been emitted.
+                    ///
+                    /// Upon receiving a `Create` event for a directory, it is necessary to scan the newly created
+                    /// directory for contents. The directory can contain files or directories if those contents
+                    /// were created before the directory could be watched, or if the directory was moved into the
+                    /// watched directory.
+                    DebouncedEvent::Write(path) => {
+                        println!("write: {}", path.display());
+                    },
+
+                    /// `Chmod` is emitted when attributes have been changed and no events were detected for the
+                    /// path within the specified time frame.
+                    DebouncedEvent::Chmod(path) => {
+                        println!("chmod: {}", path.display());
+                    },
+
+                    /// `Remove` is emitted when a file or directory has been removed and no events were detected
+                    /// for the path within the specified time frame.
+                    DebouncedEvent::Remove(path) => {
+                        println!("remove: {}", path.display());
+                    },
+
+                    /// `Rename` is emitted when a file or directory has been moved within a watched directory and
+                    /// no events were detected for the new path within the specified time frame.
+                    ///
+                    /// The first path contains the source, the second path the destination.
+                    DebouncedEvent::Rename(oldpath, newpath) => {
+                        println!("rename: {} to {}", oldpath.display(), newpath.display());
+
+                    },
+
+                    /// `Rescan` is emitted immediately after a problem has been detected that makes it necessary
+                    /// to re-scan the watched directories.
+                    DebouncedEvent::Rescan => {
+                        println!("rescan");
+                    },
+
+                    /// `Error` is emitted immediately after a error has been detected.
+                    ///
+                    ///  This event may contain a path for which the error was detected.
+                    DebouncedEvent::Error(err, optpath) => {
+                        print!("error: ");
+                        if let Some(path) = optpath {
+                            print!("({})", path.display());
+                        }
+                        print!("{}", err);
+                        println!();
+                    },
+                }
+            },
+            Err(e) => println!("watch error: {:?}", e),
+        }
     }
 }
