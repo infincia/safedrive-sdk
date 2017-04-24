f8932e338ec2c5963e21936314edea784dcd75e8
Stephen Oliver <steve@infincia.com>
2017-04-23 23:58:21 -0400

    [Build] minor script style fixes

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ if [ -z "${TARGET}" ]; then
     export TARGET=`rustup show | awk 'match($0, /Default host: ([0-9a-zA-Z\_]).+/) { ver = substr($3, RSTART, RLENGTH); print ver;}'`
 fi
 
-echo "Building release for $TARGET"
+echo "Building release for ${TARGET}"
 
 export BUILD_PREFIX=$PWD/dep/${TARGET}
 export DIST_PREFIX=$PWD/dist/${TARGET}
@@ -61,29 +61,29 @@ mkdir -p ${DIST_PREFIX}/lib
 mkdir -p ${DIST_PREFIX}/include
 mkdir -p ${DIST_PREFIX}/bin
 
-echo "Building dependencies for $TARGET"
+echo "Building dependencies for ${TARGET}"
 
 bash dep.sh
 
 source ./rustver.sh
 
-rustup override set $RUST_VER
+rustup override set ${RUST_VER}
 
-echo "Building safedrive CLI for $TARGET"
+echo "Building safedrive CLI for ${TARGET}"
 
-RUST_BACKTRACE=1 cargo build --release -p safedrive --target $TARGET > /dev/null
+RUST_BACKTRACE=1 cargo build --release -p safedrive --target ${TARGET} > /dev/null
 
-echo "Building safedrive daemon for $TARGET"
+echo "Building safedrive daemon for ${TARGET}"
 
-RUST_BACKTRACE=1 cargo build --release -p safedrived --target $TARGET > /dev/null
+RUST_BACKTRACE=1 cargo build --release -p safedrived --target ${TARGET} > /dev/null
 
-echo "Building SDDK headers for $TARGET"
+echo "Building SDDK headers for ${TARGET}"
 
 cheddar -f libsafedrive/src/c_api.rs ${DIST_PREFIX}/include/sddk.h
 
-echo "Copying build artifacts for $TARGET"
+echo "Copying build artifacts for ${TARGET}"
 
-case $TARGET in
+case ${TARGET} in
     x86_64-apple-darwin)
         cp -a target/${TARGET}/release/libsafedrive.dylib ${DIST_PREFIX}/lib/libsafedrive.dylib
         install_name_tool -id "@rpath/libsafedrive.dylib" ${DIST_PREFIX}/lib/libsafedrive.dylib
