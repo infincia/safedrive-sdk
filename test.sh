65e72755b374249491e58683cd44f6ff73f51365
Stephen Oliver <steve@infincia.com>
2017-04-25 13:56:22 -0400

    [Build] update test script linker settings for newer dist path

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -e
 
 rm test -f &> /dev/null
 
-clang -o test libsafedrive/src/test.c -rpath dist-x86_64-apple-darwin/lib -Ldist-x86_64-apple-darwin/lib -Idist-x86_64-apple-darwin/include -lz -lsodium -lsafedrive -framework Foundation -framework Security -O2
+clang -o test libsafedrive/src/test.c -rpath dist/x86_64-apple-darwin/lib -Ldist/x86_64-apple-darwin/lib -Idist/x86_64-apple-darwin/include -lz -lsodium -lsafedrive -framework Foundation -framework Security -O2
 
 time RUST_BACKTRACE=1 ./test
 
