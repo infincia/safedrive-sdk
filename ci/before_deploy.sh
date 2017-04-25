23bc3a5900429bfdb0e4af6e14bfbe57ddfd7045
Stephen Oliver <steve@infincia.com>
2017-04-25 13:54:54 -0400

    [CI] fix linux deploy paths

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -8,7 +8,7 @@ set -ex
 
 mk_tarball() {
     # release tarball will look like 'rust-everywhere-v1.2.3-x86_64-unknown-linux-gnu.tar.gz'
-    pushd dist-$TARGET/
+    pushd dist/$TARGET/
     tar -zcf ../${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}.tar.gz *
     popd
 }
@@ -21,8 +21,8 @@ mk_tarball() {
 # fully conform to Debian packaging guideliens (`lintian` raises a few warnings/errors)
 mk_deb() {
     # TODO update this part to package the artifacts that make sense for your project
-    dobin dist-$TARGET/bin/safedrive
-    dobin dist-$TARGET/bin/safedrived
+    dobin dist/$TARGET/bin/safedrive
+    dobin dist/$TARGET/bin/safedrived
 }
 
 main() {
