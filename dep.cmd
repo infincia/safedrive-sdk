50c2de6fe023d2a921969ef864722fa16d54239f
Stephen Oliver <steve@infincia.com>
2017-04-26 13:10:02 -0400

    [Build] always build libsodium if the library isn’t found in dep\

--- dep.cmd
+++ dep.cmd
@@ -48,6 +48,9 @@ IF NOT EXIST libsodium-%SODIUM_VER%.tar.gz (
 
 popd
 
+IF NOT EXIST "%BUILD_PREFIX%\lib\sodium.%LIBSUFFIX%" || goto :build
+goto :EOF
+
 findstr /c:"%SODIUM_VER%" %SODIUM_VER_FILE% > NUL || goto :build
 goto :EOF
 
