36e1d2e7144b0307a7ada2694fae579808d994e8
Stephen Oliver <steve@infincia.com>
2017-04-29 17:39:09 -0400

    [Library] add RemoteFS struct

--- sddk/src/remotefs.rs
+++ sddk/src/remotefs.rs
@@ -1,1 +1,31 @@
 #![cfg(target_os = "macos")]
+
+use ssh2::{Session, Sftp};
+use std::net::TcpStream;
+use std::path::{Path, PathBuf, Component};
+
+
+use error::SDError;
+
+
+pub struct RemoteFS<'a> {
+    host: &'a str,
+    port: u16,
+    username: &'a str,
+    password: &'a str,
+    tcp_stream: Option<TcpStream>,
+    session: Option<Session>,
+}
+
+impl<'a> RemoteFS<'a> {
+    pub fn new(host: &'a str, port: u16, username: &'a str, password: &'a str) -> RemoteFS<'a> {
+        RemoteFS {
+            host: host,
+            port: port,
+            username: username,
+            password: password,
+            tcp_stream: None,
+            session: None,
+        }
+    }
+}
