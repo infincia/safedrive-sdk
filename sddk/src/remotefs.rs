fd9514afd859d00d802e7a3c49e0ae674c104433
Stephen Oliver <steve@infincia.com>
2017-04-30 02:05:38 -0400

    [Library] add ssh connect method

--- sddk/src/remotefs.rs
+++ sddk/src/remotefs.rs
@@ -28,4 +28,52 @@ impl<'a> RemoteFS<'a> {
             session: None,
         }
     }
+
+    pub fn connect(&mut self) -> Result<(), SDError> {
+        match self.session {
+            Some(_) => {
+                debug!("reusing ssh session");
+                Ok(())
+            },
+            None => {
+                debug!("creating new ssh session");
+
+                debug!("connecting to url {}:{}", self.host, self.port);
+
+                let tcp = TcpStream::connect((self.host, self.port))?;
+
+                debug!("opening session on {:?}", tcp);
+
+                let mut session: Session = match Session::new() {
+                    Some(session) => session,
+                    None => return Err(SDError::Internal("no session available".to_string()))
+                };
+
+                debug!("running ssh handshake");
+
+                session.handshake(&tcp)?;
+
+                debug!("authenticating");
+
+                session.userauth_password(self.username, self.password)?;
+
+                if session.authenticated() {
+                    debug!("session authenticated");
+                    self.session = Some(session);
+                    self.tcp_stream = Some(tcp);
+                    Ok(())
+                } else {
+                    debug!("session authentication failed");
+
+                    self.session = None;
+                    self.tcp_stream = None;
+                    Err(SDError::Authentication)
+                }
+            }
+        }
+    }
+
+
+
 }
+
