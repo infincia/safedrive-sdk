f748d4620126e974783e0a8ce8c7953d7decd5ec
Stephen Oliver <steve@infincia.com>
2017-05-04 11:48:48 -0400

    [Wrapper] don’t alias the cdetails member in AccountDetails

--- Windows/AccountDetails.cpp
+++ Windows/AccountDetails.cpp
@@ -2,12 +2,12 @@
 #include "SafeDriveSDK.h"
 #include "sddk.h"
 
-AccountDetails::AccountDetails(SDDKAccountDetails* cdetails) {
-	assignedStorage = cdetails->assigned_storage;
-	usedStorage = cdetails->used_storage;
-	lowFreeStorageThreshold = cdetails->low_free_space_threshold;
-	expirationDate = cdetails->expiration_date;
-	cdetails = cdetails;
+AccountDetails::AccountDetails(SDDKAccountDetails* details) {
+	assignedStorage = details->assigned_storage;
+	usedStorage = details->used_storage;
+	lowFreeStorageThreshold = details->low_free_space_threshold;
+	expirationDate = details->expiration_date;
+	cdetails = details;
 }
 
 AccountDetails::~AccountDetails() {
