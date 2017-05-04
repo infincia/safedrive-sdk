a1825b9fa939f87f0f1d4aedbd9475ae211c6ab0
Stephen Oliver <steve@infincia.com>
2017-05-04 00:25:31 -0400

    [Build] simplify windows dependency script

--- dep.cmd
+++ dep.cmd
@@ -24,14 +24,12 @@ mkdir build > NUL
 
 IF "%ARCH%"=="x86_64" (
     set PLATFORM=x64
-    set SSH2_GENERATOR_PLATFORM= Win64
-    set LIBRESSL_GENERATOR_PLATFORM= Win64
+    set CMAKE_GENERATOR_PLATFORM= Win64
 )
 
 IF "%ARCH%"=="x86" (
     set PLATFORM=Win32
-    set SSH2_GENERATOR_PLATFORM=
-    set LIBRESSL_GENERATOR_PLATFORM=
+    set CMAKE_GENERATOR_PLATFORM=
 )
 
 IF "%TOOLSET%"=="v120_xp" (
@@ -123,8 +121,8 @@ del /q libressl-%LIBRESSL_VER%
 7z x -y "%SRC_PREFIX%\libressl-%LIBRESSL_VER%.tar.gz" || goto :error
 7z x -y "libressl-%LIBRESSL_VER%.tar" || goto :error
 pushd libressl-%LIBRESSL_VER%
-@echo building libressl for "!VS!!LIBRESSL_GENERATOR_PLATFORM!"
-cmake . -G"!VS!!LIBRESSL_GENERATOR_PLATFORM!" -D"BUILD_SHARED_LIBS=0" -D"BUILD_EXAMPLES=0" -D"BUILD_TESTING=0" -D"CMAKE_BUILD_TYPE=Release"
+@echo building libressl for "!VS!!CMAKE_GENERATOR_PLATFORM!"
+cmake . -G"!VS!!CMAKE_GENERATOR_PLATFORM!" -D"BUILD_SHARED_LIBS=0" -D"BUILD_EXAMPLES=0" -D"BUILD_TESTING=0" -D"CMAKE_BUILD_TYPE=Release"
 msbuild /m /v:n /p:Configuration=%CONFIGURATION%;Platform=%PLATFORM%;PlatformToolset=%TOOLSET% libressl.sln || goto :error
 @echo copying "include\openssl" to "%BUILD_PREFIX%\include\"
 copy /y "include\openssl"  "%BUILD_PREFIX%\include\openssl\" || goto :error
@@ -157,8 +155,8 @@ del /q libssh2-%LIBSSH2_VER%
 7z x -y "%SRC_PREFIX%\libssh2-%LIBSSH2_VER%.tar.gz" || goto :error
 7z x -y "libssh2-%LIBSSH2_VER%.tar" || goto :error
 pushd libssh2-%LIBSSH2_VER%
-@echo building libssh2 for "!VS!!LIBRESSL_GENERATOR_PLATFORM!"
-cmake . -G"!VS!!SSH2_GENERATOR_PLATFORM!" -D"BUILD_SHARED_LIBS=0" -D"BUILD_EXAMPLES=0" -D"BUILD_TESTING=0" -D"CMAKE_BUILD_TYPE=Release" -D"OPENSSL_USE_STATIC_LIBS=TRUE" -D"CRYPTO_BACKEND=OpenSSL" -D"OPENSSL_ROOT_DIR="%BUILD_PREFIX%\\" -D"OPENSSL_INCLUDE_DIR="%BUILD_PREFIX%\include\\"
+@echo building libssh2 for "!VS!!CMAKE_GENERATOR_PLATFORM!"
+cmake . -G"!VS!!CMAKE_GENERATOR_PLATFORM!" -D"BUILD_SHARED_LIBS=0" -D"BUILD_EXAMPLES=0" -D"BUILD_TESTING=0" -D"CMAKE_BUILD_TYPE=Release" -D"OPENSSL_USE_STATIC_LIBS=TRUE" -D"CRYPTO_BACKEND=OpenSSL" -D"OPENSSL_ROOT_DIR="%BUILD_PREFIX%\\" -D"OPENSSL_INCLUDE_DIR="%BUILD_PREFIX%\include\\"
 msbuild /m /v:n /p:OutDir="%BUILD_PREFIX%\lib\\";Configuration=%CONFIGURATION%;Platform=%PLATFORM%;PlatformToolset=%TOOLSET% libssh2.sln || goto :error
 popd
 del /q libssh2-%LIBSSH2_VER%
