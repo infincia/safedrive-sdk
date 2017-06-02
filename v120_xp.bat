c704519e57c4d6bbc27a5ce873b075c2a427e93c
Stephen Oliver <steve@infincia.com>
2017-06-01 22:27:21 -0400

    [Build] remove old toolchain settings files

--- v120_xp.bat
+++
@@ -1,5 +0,0 @@
-set INCLUDE=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Include;%INCLUDE%
-set PATH=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Bin;%PATH%
-set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Lib;%LIB%
-set CL=/D_USING_V120_SDK71_;%CL%
-set LINK=/SUBSYSTEM:CONSOLE,5.01 %LINK%
--- v140_xp64.bat
+++
@@ -1,6 +0,0 @@
-set INCLUDE=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Include;%INCLUDE%
-set PATH=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Bin;%PATH%
-set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Lib\x64;%LIB%
-set CL=/D_USING_V140_SDK71_;%CL%
-set LINK=/SUBSYSTEM:CONSOLE,5.02 %LINK%
-
