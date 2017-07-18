7c6ae3f5f612599b076ab59477c43760d4ad6759
Stephen Oliver <steve@infincia.com>
2017-07-18 11:40:05 -0400

    [Project] remove safedrived from the tree, it's separate now

--- safedrived/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    if cfg!(target_os = "macos") {
-        println!("cargo:rustc-link-args=-mmacosx-version-min=10.9");
-    }
-    println!("cargo:rustc-link-lib=static=sodium");
-}
-
