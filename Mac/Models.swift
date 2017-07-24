d96a7137c68ec8774e327d68d84705289cab42fc
Stephen Oliver <steve@infincia.com>
2017-07-23 20:34:36 -0400

    [Swift] make SDKSyncFolder thread-safe and support transport through XPC

--- Mac/Models.swift
+++ Mac/Models.swift
@@ -38,23 +38,150 @@ public enum SDKSyncType {
     case unencrypted
 }
 
-public struct SDKSyncFolder: Equatable {
-    public var id: UInt64
-    public var name: String
-    public var path: String
-    public var date: Date
-    public var encrypted: Bool
-    public var type: SDKSyncType
+public class SDKSyncFolder: Equatable, NSSecureCoding {
+    fileprivate let modificationQueue = DispatchQueue(label: "io.safedrive.SafeDriveSDK.SDKSyncFolder.modificationQueue")
+
+    public static var supportsSecureCoding = true
+
+    var _id: UInt64
+    var _name: String
+    var _path: String
+    var _date: Date
+    var _encrypted: Bool
     
     // whether the folder should be allowed to sync, is it disabled/missing etc
-    public var active: Bool = true
+    var _active: Bool = true
     
-    public var syncFrequency: String = "daily"
+    var _syncFrequency: String = "daily"
     
-    public var syncTime: Date
+    var _syncTime: Date
     
     public var url: URL? {
-        return URL(fileURLWithPath: path, isDirectory: true)
+        return URL(fileURLWithPath: _path, isDirectory: true)
+    }
+    
+    public var type: SDKSyncType {
+        return self.encrypted ? .encrypted : .unencrypted
+    }
+    
+    public var id: UInt64 {
+        get {
+            var r: UInt64 = 0
+            modificationQueue.sync {
+                r = self._id
+            }
+            return r
+        }
+        set (newValue) {
+            modificationQueue.sync(flags: .barrier, execute: {
+                self._id = newValue
+            })
+        }
+    }
+    
+    public var name: String {
+        get {
+            var r: String = ""
+            modificationQueue.sync {
+                r = self._name
+            }
+            return r
+        }
+        set (newValue) {
+            modificationQueue.sync(flags: .barrier, execute: {
+                self._name = newValue
+            })
+        }
+    }
+    
+    public var path: String {
+        get {
+            var r: String = ""
+            modificationQueue.sync {
+                r = self._path
+            }
+            return r
+        }
+        set (newValue) {
+            modificationQueue.sync(flags: .barrier, execute: {
+                self._path = newValue
+            })
+        }
+    }
+    
+    public var date: Date {
+        get {
+            var r: Date!
+            modificationQueue.sync {
+                r = self._date
+            }
+            return r
+        }
+        set (newValue) {
+            modificationQueue.sync(flags: .barrier, execute: {
+                self._date = newValue
+            })
+        }
+    }
+    
+    public var encrypted: Bool {
+        get {
+            var r: Bool = false
+            modificationQueue.sync {
+                r = self._encrypted
+            }
+            return r
+        }
+        set (newValue) {
+            modificationQueue.sync(flags: .barrier, execute: {
+                self._encrypted = newValue
+            })
+        }
+    }
+
+    public var active: Bool {
+        get {
+            var r: Bool = false
+            modificationQueue.sync {
+                r = self._active
+            }
+            return r
+        }
+        set (newValue) {
+            modificationQueue.sync(flags: .barrier, execute: {
+                self._active = newValue
+            })
+        }
+    }
+    
+    public var syncFrequency: String {
+        get {
+            var r: String = ""
+            modificationQueue.sync {
+                r = self._syncFrequency
+            }
+            return r
+        }
+        set (newValue) {
+            modificationQueue.sync(flags: .barrier, execute: {
+                self._syncFrequency = newValue
+            })
+        }
+    }
+    
+    public var syncTime: Date {
+        get {
+            var r: Date!
+            modificationQueue.sync {
+                r = self._syncTime
+            }
+            return r
+        }
+        set (newValue) {
+            modificationQueue.sync(flags: .barrier, execute: {
+                self._syncTime = newValue
+            })
+        }
     }
     
     public func exists() -> Bool {
@@ -73,20 +200,74 @@ public struct SDKSyncFolder: Equatable {
     }
     
     public init(folder: SDDKFolder) {
-        self.id = folder.id
-        self.name = String(cString: folder.name)
-        self.path = String(cString: folder.path)
-        self.date = Date(timeIntervalSince1970: Double(folder.date) / 1000)
-        self.encrypted = folder.encrypted == 1 ? true : false
-        self.active = folder.syncing == 1 ? true : false
-        self.type = self.encrypted ? .encrypted : .unencrypted
+        _id = folder.id
+        _name = String(cString: folder.name)
+        _path = String(cString: folder.path)
+        _date = Date(timeIntervalSince1970: Double(folder.date) / 1000)
+        _encrypted = folder.encrypted == 1 ? true : false
+        _active = folder.syncing == 1 ? true : false
         
         // TODO: get this from SDDKFolder once it has that property
         var components = DateComponents()
         components.hour = 0
         components.minute = 0
         let calendar = Calendar.current
-        self.syncTime = calendar.date(from: components)!
+        _syncTime = calendar.date(from: components)!
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        guard let id = aDecoder.decodeObject(of: NSNumber.self, forKey: "id") as? UInt64 else {
+            fatalError("Could not deserialise id!")
+        }
+        
+        guard let path = aDecoder.decodeObject(of: NSString.self, forKey: "path") as String? else {
+            fatalError("Could not deserialise path!")
+        }
+        
+        guard let name = aDecoder.decodeObject(of: NSString.self, forKey: "name") as String? else {
+            fatalError("Could not deserialise name!")
+        }
+        
+        guard let date = aDecoder.decodeObject(of: NSDate.self, forKey: "date") as Date? else {
+            fatalError("Could not deserialise date!")
+        }
+
+        guard let encrypted = aDecoder.decodeObject(of: NSNumber.self, forKey: "encrypted") as? Bool else {
+            fatalError("Could not deserialise encrypted!")
+        }
+        
+        guard let active = aDecoder.decodeObject(of: NSNumber.self, forKey: "active") as? Bool else {
+            fatalError("Could not deserialise active!")
+        }
+        
+        guard let syncFrequency = aDecoder.decodeObject(of: NSString.self, forKey: "syncFrequency") as String? else {
+            fatalError("Could not deserialise syncFrequency!")
+        }
+        
+        guard let syncTime = aDecoder.decodeObject(of: NSDate.self, forKey: "syncTime") as Date? else {
+            fatalError("Could not deserialise syncTime!")
+        }
+        
+        
+        _id = id
+        _name = name
+        _path = path
+        _date = date
+        _encrypted = encrypted
+        _active = active
+        _syncFrequency = syncFrequency
+        _syncTime = syncTime
+    }
+    
+    public func encode(with aCoder: NSCoder) {
+        aCoder.encode(id, forKey: "id")
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(path, forKey: "path")
+        aCoder.encode(date, forKey: "date")
+        aCoder.encode(encrypted, forKey: "encrypted")
+        aCoder.encode(active, forKey: "active")
+        aCoder.encode(syncFrequency, forKey: "syncFrequency")
+        aCoder.encode(syncTime, forKey: "syncTime")
     }
 }
 
