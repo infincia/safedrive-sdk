be3638f99c92ff31815967977d06179882c7a5ee
Stephen Oliver <steve@infincia.com>
2017-07-24 13:45:31 -0400

    [Build] set cargo path in release script

--- release.sh
+++ release.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export PATH=${HOME}/.cargo/bin:${PATH}
+
 if [ -z "${TARGET}" ]; then
     export TARGET=`rustup show | awk 'match($0, /Default host: ([0-9a-zA-Z\_]).+/) { ver = substr($3, RSTART, RLENGTH); print ver;}'`
 fi
