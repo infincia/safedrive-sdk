6ce2d3e36593fa1c69b156091a016c791dd7560a
Stephen Oliver <steve@infincia.com>
2017-08-19 19:28:12 -0400

    [Build] always set build properties in windows scripts

--- release.cmd
+++ release.cmd
@@ -7,14 +7,24 @@ set CONFIGURATION=%2
 set TOOLSET=%3
 set CARGO_INCREMENTAL=1
 
-
 IF "!PLATFORM!"=="x64" (
     set TARGET=x86_64-pc-windows-msvc
+    set ARCH=x64
+    set VS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat
+    set TOOLSET=v141_xp
+    set ESCRIPT=v141_xp64.bat
 )
 
 IF "!PLATFORM!"=="Win32" (
     set TARGET=i686-pc-windows-msvc
+    set ARCH=x86
+    set VS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat
+    set TOOLSET=v141_xp
+    set ESCRIPT=v141_xp.bat
 )
+
+if defined VS call "%VS%" %ARCH%
+if defined ESCRIPT call "%ESCRIPT%"
 
 rustup target add !TARGET! > NUL 2>&1
 
