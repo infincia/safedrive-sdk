4d599cedf3cfb7ffb2243a508e86a5bc84d32365
Stephen Oliver <steve@infincia.com>
2017-08-19 23:42:24 -0400

    [CI] fix install.cmd

--- ci/install.cmd
+++ ci/install.cmd
@@ -1,12 +1,16 @@
 
 setlocal enabledelayedexpansion
 
-set PATH="C:\Users\%USER%\.cargo\bin;C:\Program Files\Git\bin;C:\Program Files\Git\mingw64\bin;C:\Program Files\7-Zip;C:\Program Files\WinAnt;%PATH%;"
+set PATH=%USERPROFILE%\.cargo\bin;C:\Program Files\Git\bin;C:\Program Files\Git\mingw64\bin;C:\Program Files\7-Zip;C:\Program Files\WinAnt;%PATH%
 
 curl -sSf -o rustup-init.exe https://win.rustup.rs
+
 rustup-init.exe --default-host %TARGET% -y
+
 rustc -Vv
+
 cargo -V
+
 if not exist "C:\Users\%USER%\.cargo\bin\cheddar.exe" cargo install moz-cheddar
 
 goto :done
