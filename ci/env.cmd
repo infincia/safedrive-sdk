2896c3914e65b9a87848adb977ade95d55f153e4
Stephen Oliver <steve@infincia.com>
2017-08-20 12:59:55 -0400

    [CI] fix path quotes in windows env script

--- ci/env.cmd
+++ ci/env.cmd
@@ -10,7 +10,7 @@ set CARGO_INCREMENTAL=1
 
 IF "!PLATFORM!"=="x64" (
     set ARCH=x64
-    set VS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat
+    set VS="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"
     set TOOLSET=v141_xp
     set ESCRIPT=v141_xp64.bat
     set CONFIGURATION=Release
@@ -18,11 +18,11 @@ IF "!PLATFORM!"=="x64" (
 
 IF "!PLATFORM!"=="Win32" (
     set ARCH=x86
-    set VS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat
+    set VS="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"
     set TOOLSET=v141_xp
     set ESCRIPT=v141_xp.bat
     set CONFIGURATION=Release
 )
 
-if defined VS call "%VS%" %ARCH%
+if defined VS call %VS% %ARCH%
 if defined ESCRIPT call "%ESCRIPT%"
